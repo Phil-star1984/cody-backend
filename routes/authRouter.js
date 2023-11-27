2beebf88ac7da792df0037114e9206af887aba76
Philipp Mulfinger <info@phil-splash.de>
2023-11-27 19:57:09 +0100

    Added verifyToken

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,9 +1,11 @@
 import express from "express";
 import * as authController from "../controller/authController.js";
 const authRouter = express.Router();
+import verifyToken from "../middlewares/verifyToken.js";
 
 authRouter
   .post("/signup", authController.signUp)
-  .get("/:id", authController.getUser);
+  .post("/signin", authController.signIn)
+  .get("/me", verifyToken, authController.getUser);
 
 export default authRouter;
