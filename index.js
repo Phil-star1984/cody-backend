85756db106db0e94b09590ada2ae85f7b14aca6e
Philipp Mulfinger <info@phil-splash.de>
2023-11-28 17:46:24 +0100

    Added logout

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ import "./db/mongoDB.js";
 const PORT = process.env.PORT || 5008;
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
